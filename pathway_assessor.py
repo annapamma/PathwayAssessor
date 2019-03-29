0d3bb238e79378d26d7bc0dd1f7bab49733e627f
Anna Calinawan <anna.pamela@gmail.com>
2019-03-29 15:41:09 -0400

    reads expression table and averages duplicate rows

--- pathway_assessor.py
+++ pathway_assessor.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
 
-def expression_table(expression_table_f):
-    pass
+def expression_table(f):
+    df = pd.read_csv(f, sep='\t', header=0, index_col=0)
+    df.index.name = 'genes'
+    return df.groupby('genes').mean()
+
--- tests/test_pathway_assessor.py
+++ tests/test_pathway_assessor.py
@@ -12,15 +12,21 @@ import pathway_assessor as _
 class TestPathwayAssessor(unittest.TestCase):
 
     def setUp(self):
-        self.expression_table_f = 'tests/expression_table_na.tsv'
+        self.expression_table_f = 'tests/expression_table.tsv'
         self.expression_table = _.expression_table(self.expression_table_f)
 
     # sanity check
     def test_hello_world_returns_str(self):
         self.assertIsInstance('hello world', str)
 
-    def test_expression_table_is_dataframe(self):
+    def test_expression_table_is_dataframe_of_expected_values(self):
         self.assertIsInstance(self.expression_table, pd.DataFrame)
+        self.assertAlmostEqual(self.expression_table['Sample_A'].loc['PIKFYVE'], 7.442)
+        self.assertAlmostEqual(self.expression_table['Sample_B'].loc['PIKFYVE'], 10.969)
+        self.assertAlmostEqual(self.expression_table['Sample_C'].loc['PIKFYVE'], 5.520)
+        self.assertTrue(pd.isna(self.expression_table['Sample_C'].loc['PHOSPHO1']))
+
+
 
 if __name__ == '__main__':
     unittest.main()
