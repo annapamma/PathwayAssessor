11ee13f56a5795b658513697816fa5fc5b8c0234
Anna Calinawan <anna.pamela@gmail.com>
2019-03-29 15:34:29 -0400

    first expected test failure

---
+++ pathway_assessor.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+def expression_table(expression_table_f):
+    pass
--- tests/test_pathway_assessor.py
+++ tests/test_pathway_assessor.py
@@ -1,12 +1,26 @@
 import unittest
 import sys
 
+import pandas as pd
+
+from os import path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+import pathway_assessor as _
+
 
 class TestPathwayAssessor(unittest.TestCase):
+
+    def setUp(self):
+        self.expression_table_f = 'tests/expression_table_na.tsv'
+        self.expression_table = _.expression_table(self.expression_table_f)
+
     # sanity check
     def test_hello_world_returns_str(self):
         self.assertIsInstance('hello world', str)
 
+    def test_expression_table_is_dataframe(self):
+        self.assertIsInstance(self.expression_table, pd.DataFrame)
 
 if __name__ == '__main__':
     unittest.main()
