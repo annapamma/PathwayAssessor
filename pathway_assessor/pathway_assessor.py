b39414efab11135607b28803e743ed02ec0f28e2
Anna Calinawan <anna.pamela@gmail.com>
2019-04-05 14:53:36 -0400

    removes unused fn

--- pathway_assessor/pathway_assessor.py
+++ pathway_assessor/pathway_assessor.py
@@ -10,12 +10,6 @@ import pandas as pd
 import scipy.stats as stats
 
 
-# def expression_table(f):
-#     df = pd.read_csv(f, sep='\t', header=0, index_col=0)
-#     df.index.name = 'genes'
-#     return df.groupby('genes').mean()
-
-
 def processed_expression_table(df):
     df.index.name = 'genes'
     return df.groupby('genes').mean()
